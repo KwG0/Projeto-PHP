5d6d848ec28f43b3f6822589149bbcf8e80e3a37
Kauanne <contatokahg01@gmail.com>
2022-07-18 18:13:23 -0300

    Correção do Header()

--- script.php
+++ script.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-include (index.php);
 
 $categorias = [];
 $categorias[] = 'Infantil';
@@ -12,28 +11,28 @@ $idade = $_POST['idade'];
 
 if(empty($nome)){
     $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio!';
-    location('index.php');
-    return;
-}
-//resolver o location
-/*if(strlen($nome) < 3){
+    header("Location: index.php");
+    die();
+
+}else if(strlen($nome) < 3){
     $_SESSION['mensagem-de-erro'] = 'O nome não pode conter menos que 3 caracteres';
-    header[string "location: index.php"];
-    return;
-}
+    header("Location: index.php");
+    die();
 
-if(strlen($nome) > 40){
+}else if(strlen($nome) > 40){
     $_SESSION['mensagem-de-erro'] = 'Calma lá! O nome é muito extenso';
-    header[string "location: Natacao.php"];
-}
-if(is_numeric($nome)){
-    $_SESSION['mensagem-de-erro'] = 'Não digite números em seu nome!';
-    header[string "location: Natacao.php"];
-}
+    header("Location: index.php");
+    die();
 
-if(!is_numeric($idade)){
+}else if(is_numeric($nome)){
+    $_SESSION['mensagem-de-erro'] = 'Não digite números em seu nome!';
+    header("Location: index.php");
+    die();
+
+}else if(!is_numeric($idade)){
     $_SESSION['mensagem-de-erro'] = 'Informe sua idade em números';
-    header[string "location: Natacao.php"];
+    header("Location: index.php");
+    die();
 }
 
 if($idade >= 6 && $idade <= 12){
