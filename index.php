b44ee2cb0dfac96d6227ea2f110e2bf2a42820d8
Kauanne <contatokahg01@gmail.com>
2022-07-18 20:28:05 -0300

    Estilização simples

--- index.php
+++ index.php
@@ -10,18 +10,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Inscrição</title>
 </head>
+<style>
+    .container {
+        color: peachpuff;
+        display: flex;
+        flex-direction: initial;
+        justify-content: space-around;
+        padding: 50px 70px 60px;
+        background-color: white;
+    }
+    h3 {
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+    .forms{
+        background-color: white;
+        padding: 10px;
+        border: 3px solid whitesmoke;
+        border-style: dotted;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .btt{
+        border:none;
+        background-color: lightskyblue;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        align-items: center;
+    }
+
+</style>
 <body>
-    <h3>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</h3>
-    <form action="../Natação/script.php" method="post">
-        <?php
-            $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
-            if(!empty($mensagemDeErro)) {
+    <div class="container">
+        <h3>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</h3>
+        <div class="forms">
+             <form action="../Natação/script.php" method="post" class="inputs">
+            <?php
+             $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
+                if(!empty($mensagemDeErro)) {
                 echo $mensagemDeErro;
-            }
-        ?>
-        <p>Seu Nome: <input type="text" name="nome"/></p>
-        <p>Sua Idade: <input type="text" name="idade"/></p>
-        <p><input type="submit" value="Enviar dados"/></p>
-    </form>
+                }
+            ?>
+            <p>Seu Nome: <input type="text" name="nome"/></p>
+            <p>Sua Idade: <input type="text" name="idade"/></p>
+            <input type="submit" value="Enviar dados" class="btt"/>
+        </form>
+        </div>
+    </div>
 </body>
 </html>
