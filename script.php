e3550d833fdfa99ef5f4124928949decc5c4ff90
Kauanne <contatokahg01@gmail.com>
2022-07-18 21:31:01 -0300

    Alteração no sistema de validação de dados

--- script.php
+++ script.php
@@ -38,20 +38,29 @@ if(empty($nome)){
 if($idade >= 6 && $idade <= 12){
     //echo 'Infantil';
     for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Infantil')
-            echo "O nadador " .$nome. " compete na categoria Infantil";
+        if($categorias[$I] == 'Infantil'){
+            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Infantil";
+            header("Location: index.php");
+            die();
+        }
     }
 }else if($idade >= 13 && $idade <= 18){
     //echo 'Adolescente';
     for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Adolescente')
-            echo "O nadador " .$nome. " compete na categoria Adolescente";
+        if($categorias[$I] == 'Adolescente'){
+            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Adolescente";
+            header("Location: index.php");
+            die();
+        }
     }
 }else{
     //echo 'Adulto';
     for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Adulto')
-            echo "O nadador " .$nome. " compete na categoria Adulto";
+        if($categorias[$I] == 'Adulto'){
+            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Adulto";
+            header("Location: index.php");
+            die();
+        }
     }
 }
 ?>
