a6ab1cf640bcb4bbded646214cb472a4929accbe
Kauanne <contatokahg01@gmail.com>
2022-07-20 19:30:53 -0300

    transferindo code para melhor funcionamento

--- script.php
+++ script.php
@@ -1,41 +1,7 @@
 <?php
 session_start();
 
-$categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
-
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-
-if($idade >= 6 && $idade <= 12){
-    //echo 'Infantil';
-    for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Infantil'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Infantil";
-            header("Location: index.php");
-            die();
-        }
-    }
-}else if($idade >= 13 && $idade <= 18){
-    //echo 'Adolescente';
-    for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Adolescente'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Adolescente";
-            header("Location: index.php");
-            die();
-        }
-    }
-}else{
-    //echo 'Adulto';
-    for($I = 0; $I <= count($categorias); $I++){
-        if($categorias[$I] == 'Adulto'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador " .$nome. " compete na categoria Adulto";
-            header("Location: index.php");
-            die();
-        }
-    }
-}
 ?>
---
+++ servicos/categorias.php
@@ -0,0 +1,36 @@
+<?php
+
+function definirCategoria(string $nome, string $idade) : ?string {
+   
+    $categorias = [];
+    $categorias[] = 'Infantil';
+    $categorias[] = 'Adolescente';
+    $categorias[] = 'Adulto';
+
+    if(validaNome($nome) && validaIdade($idade)){
+        if($idade >= 6 && $idade <= 12){
+            for($I = 0; $I <= count($categorias); $I++){
+                if($categorias[$I] == 'Infantil'){
+                    setMensagemdeSucesso(mensagem: "O nadador " .$nome. " compete na categoria Infantil");
+                    return null;
+                }
+            }
+        }else if($idade >= 13 && $idade <= 18){
+            for($I = 0; $I <= count($categorias); $I++){
+                if($categorias[$I] == 'Adolescente'){
+                    setMensagemdeSucesso(mensagem: "O nadador " .$nome. " compete na categoria Adolescente");
+                    return null;
+                }
+            }
+        }else{
+            for($I = 0; $I <= count($categorias); $I++){
+                if($categorias[$I] == 'Adulto'){
+                    setMensagemdeSucesso(mensagem: "O nadador " .$nome. " compete na categoria Adulto");
+                    return null;
+                }
+            }
+        }
+    }else{
+        return obterMensagemdeErro();
+    }
+}
